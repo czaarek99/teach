4df18f7bdd4944c01f67c41b17142755615227da
Czarek <sarek123456789@gmail.com>
2019-06-09 19:27:58 +0200

    Small changes

--- services/image-service/src/Server.ts
+++ services/image-service/src/Server.ts
@@ -1,10 +1,17 @@
 import * as Koa from "koa";
 import { config } from "./config";
+import { Logger } from "server-lib";
 
 export class Server {
 
+	private readonly logger = new Logger("global-logger");
+
 	public startServer() : void {
 		const app = new Koa();
+
+		this.logger.info("Starting server", {
+			port: config.serverPort
+		});
 
 		app.listen(config.serverPort);
 	}
--- services/image-service/src/config.ts
+++ services/image-service/src/config.ts
@@ -1,3 +1,5 @@
 export const config = {
-	serverPort: process.env.SERVER_PORT
+	serverPort: process.env.SERVER_PORT,
+	isDevelopment: process.env.NODE_ENV === "development",
+	isProduction: process.env.NODE_ENV === "production"
 }
